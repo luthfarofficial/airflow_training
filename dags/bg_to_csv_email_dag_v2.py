798947b5314f5e7364bb8ca73f4808421ddf205c
Luthfa Rasha <luthfa.r@Luthfas-MacBook-Pro.local>
2025-10-20 23:38:36 +0100

    use dag and task decorators

--- dags/bg_to_csv_email_dag_v2.py
+++ dags/bg_to_csv_email_dag_v2.py
@@ -1,5 +1,5 @@
 from airflow.providers.google.cloud.operators.bigquery import BigQueryGetDataOperator
-from airflow import DAG
+from airflow.decorators import dag, task
 from datetime import datetime
 import pandas as pd
 import os
@@ -14,59 +14,51 @@ TABLE_NAME = "event_config"
 
 CSV_FILE_PATH = os.path.join(os.getcwd(), 'event_config.csv')
 
-
-def test_slack():
-    conn = BaseHook.get_connection("slack_conn_id") # receiving the connection info from metadatabase
-    client = WebClient(token=conn.password)
-    client.chat_postMessage(
-        channel="#airflow-file", 
-        text="Slack test message from Airflow!"
-)
-    
-def save_to_csv(**context):
-    data = context['task_instance'].xcom_pull(task_ids='get_data')
-    df = pd.DataFrame(data)
-    # get current working directory, file name
-    output_path = os.path.join(os.getcwd(), 'event_config.csv')
-    df.to_csv(output_path, index=False)
-    print(f"Data saved to {output_path}")
-
-
-
-
-with DAG(
+@dag(
     dag_id="bg_to_csv_email_dag_v2",
     start_date=datetime(2025, 1, 1),
-    schedule="@daily",
+    schedule="0 * * * *",
     catchup=False,
     params={
         "slack_channel":'C09LX74LM7B'
     },
-) as dag:
+)
+def bg_to_csv_dag():
+
     get_data = BigQueryGetDataOperator(
-        task_id="get_data",
-        project_id=PROJECT_ID,
-        dataset_id=DATASET_NAME,
-        table_id=TABLE_NAME,
-        gcp_conn_id="gcp_conn_id",
-    )
-    save_csv = PythonOperator(
-        task_id='save_to_csv',
-        python_callable=save_to_csv,
+    task_id="get_data",
+    project_id=PROJECT_ID,
+    dataset_id=DATASET_NAME,
+    table_id=TABLE_NAME,
+    gcp_conn_id="gcp_conn_id",
     )
 
-    test_slack_task = PythonOperator(
-        task_id="test_slack",
-        python_callable=test_slack,
-    )
+    @task
+    def save_to_csv(**context):
+        data = context['task_instance'].xcom_pull(task_ids='get_data')
+        df = pd.DataFrame(data)
+        # get current working directory, file name
+        output_path = CSV_FILE_PATH
+        df.to_csv(output_path, index=False)
+        print(f"Data saved to {output_path}")
 
-  
+    @task
+    def test_slack():
+        conn = BaseHook.get_connection("slack_conn_id") # receiving the connection info from metadatabase
+        client = WebClient(token=conn.password)
+        client.chat_postMessage(
+            channel="#airflow-file", 
+            text="Slack test message from Airflow DAG settings"
+    )
+    
     upload_file_from_path = SlackAPIFileOperator(
-        task_id="upload_file_from_path",
-        slack_conn_id="slack_conn_id",
-        channel="{{ params.slack_channel }}",
-        filename="event_config.csv",
-        initial_comment="Here is the file from the Airflow task!",
+    task_id="upload_file_from_path",
+    slack_conn_id="slack_conn_id",
+    channel="{{ params.slack_channel }}",
+    filename="event_config.csv",
+    initial_comment="Slack test message from Airflow DAG settings",
     )
 
-    get_data >> save_csv >> test_slack_task >> upload_file_from_path
+    get_data >> save_to_csv() >> test_slack() >> upload_file_from_path
+
+bg_to_csv_dag()
